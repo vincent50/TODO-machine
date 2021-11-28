3622d3b4b4683fe0cb1f36a82cc843bb086c5a3e
Felipe Sanchez <fel.snchz@gmail.com>
2021-11-28 15:31:00 -0300

    adding styles button and list

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -4,7 +4,7 @@ import './CreateTodoButton.css'
 
 export default function CreateTodoButton() {
   return (
-    <button type="button">
+    <button className="CreateTodoButton" type="button">
       +
     </button>
   )
